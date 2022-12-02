5592fbead8716d6224cf040dbd349b18328db167
artisan <artisan@amazon.com>
2022-12-02 16:47:03 -0500

    add more examples

--- examples/fix_markdown.py
+++ examples/fix_markdown.py
@@ -16,9 +16,6 @@ gpt = GPT(engine="text-davinci-003",
 )
 
 
-
-
-
 # corrected_text = openai.Completion.create(
 #     engine="text-davinci-002",
 #     prompt=text,
@@ -34,13 +31,41 @@ gpt.add_example(Example('Me no write so good',
                         'I do not know how to write well.'))
 gpt.add_example(Example('wHat Is YOUR name',
                         'What is your name?'))
-gpt.add_example(Example('Click the button',
-                        'Choose the button'))
+#Examples of inclusive language
+gpt.add_example(Example('Click yes',
+                        'Choose **Yes**'))
 gpt.add_example(Example('Click OK',
                         'Select OK.'))
+#Define button styles
 gpt.add_example(Example('Next button',
                         '<span style="ssb_orange; white-space: nowrap">Next</span> button'))
-         
+#Standardize headings for tasks and subtasks
+gpt.add_example(Example('## View the live streaming workflow',
+                        '## Task 1: View the live stream'))
+gpt.add_example(Example('### Watch The Live Stream',
+                        '### Task 1.1: Watch the live stream'))
+gpt.add_example(Example('### Create a Topic and Subscription in Amazon SNS',
+                        '### Task 2.1: Create a topic and subscription in Amazon SNS'))
+#Examples of tabs placed inside ** **
+gpt.add_example(Example('Click the Destinations tab',
+                        'Select the **Destinations** tab'))
+gpt.add_example(Example('Open the Networking tab',
+                        'Open the **Networking** tab'))
+#Examples of using the search bar
+gpt.add_example(Example('Go to the **Simple Notification Service** console.',
+                        'At the top of the AWS Management Console, in the search bar, search for and choose `Simple Notification Service`.'))
+gpt.add_example(Example('Open the **EC2** console.',
+                        'At the top of the AWS Management Console, in the search bar, search for and choose `EC2`.'))
+gpt.add_example(Example('In the search box to the right of <i class="fas fa-th"></i> **Services**, search for and choose **IAM** to open the IAM console.',
+                        'At the top of the AWS Management Console, in the search bar, search for and choose `IAM`.'))
+#Examples of using correct names for services
+gpt.add_example(Example('Cloudfront is a global content delivery network',
+                        '**Amazon Cloudfront** is a global content delivery network'))
+gpt.add_example(Example('Cloudwatch is a managed monitoring service for resources and applications in the cloud',
+                        '**Amazon Cloudwatch** is a managed monitoring service for resources and applications in the cloud'))
+gpt.add_example(Example('DynamoDB is a managed NoSQL database',
+                        '**Amazon DynamoDB** is a managed NoSQL database'))
+
 
 # Define UI configuration
 config = UIConfig(description="Insert Markdown here",
